3e529a8fe6bd6f5bb8086aed165398e37a937c69
cdnout <cdnout@gmail.com>
2020-08-24 01:25:57 +0500

    article added

---
+++ cdn/lazy-ads/var.php
@@ -0,0 +1,32 @@
+<?php 
+$file_cnn = "https://cdnjs.cloudflare.com/ajax/libs/lazyad-loader/1.1.11/lazyad-loader.min.js
+https://cdnjs.cloudflare.com/ajax/libs/lazyad-loader/1.1.11/lazyad-loader.js";
+$file_cnn = explode("https://", $file_cnn);
+
+foreach($file_cnn as $filecn){
+   
+  if(!empty($filecn)){
+    $listfiles_ar = substr($filecn, strrpos($filecn, '/') + 1);
+    $listfiles_ar = preg_replace('/\s+/', '', $listfiles_ar);
+    $listfiles_arr[] = $listfiles_ar;
+  }
+}
+
+ 
+$listfiles = $listfiles_arr;
+$title = "Lazy Ads Loader";
+$prname = "lazy-ads"; // npm command
+$keyfiles = array("lazyad-loader.min.js");
+$github = "https://github.com/madgex/lazy-ads";
+$gitrg = ""; // .tar.gz
+$keywords = array("lazy ads loading", "$prname", "$title cdn", "$title cdnout", "$title npm", "Download $title", "$title github", "Deliver synchronous ads asynchronously with RWD support without modifying the ad code."); 
+$type_c = "js";
+$npmrg = "https://registry.npmjs.org/$prname/-/$prname-"; 
+$version_limit = "-3"; // give negative value 
+//$version_lock = "locked"; // cdn/index.php for older versions will not replaced.
+$zip_remake = "true"; // will create zips for all version
+//$additional_dir = "src"; 
+//$version_lock = "locked"; // cdn/index.php for older versions will not replaced.
+//$additional_dir; for all versions 
+?>
+
---
+++ cdn/load-awesome/var.php
@@ -0,0 +1,137 @@
+<?php 
+$file_cnn = "https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/loaders.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-8bits.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-8bits.min.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-atom.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-atom.min.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-beat.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-beat.min.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-circus.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-circus.min.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-climbing-dot.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-climbing-dot.min.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-clip-rotate-multiple.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-clip-rotate-multiple.min.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-clip-rotate-pulse.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-clip-rotate-pulse.min.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-clip-rotate.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-clip-rotate.min.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-elastic-dots.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-elastic-dots.min.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-fall.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-fall.min.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-fussion.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-fussion.min.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-grid-beat.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-grid-beat.min.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-grid-pulse.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-grid-pulse.min.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-newton-cradle.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-newton-cradle.min.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-pulse-rise.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-pulse-rise.min.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-pulse-sync.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-pulse-sync.min.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-pulse.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-pulse.min.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-rotate.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-rotate.min.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-running-dots.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-running-dots.min.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-scale-multiple.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-scale-multiple.min.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-scale-pulse.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-scale-pulse.min.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-scale-ripple-multiple.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-scale-ripple-multiple.min.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-scale-ripple.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-scale-ripple.min.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-scale.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-scale.min.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-spin-clockwise-fade-rotating.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-spin-clockwise-fade-rotating.min.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-spin-clockwise-fade.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-spin-clockwise-fade.min.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-spin-clockwise.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-spin-clockwise.min.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-spin-fade-rotating.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-spin-fade-rotating.min.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-spin-fade.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-spin-fade.min.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-spin-rotate.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-spin-rotate.min.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-spin.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-spin.min.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-square-clockwise-spin.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-square-clockwise-spin.min.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-square-spin.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-square-spin.min.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-triangle-path.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-triangle-path.min.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-zig-zag-deflect.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-zig-zag-deflect.min.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-zig-zag.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/ball-zig-zag.min.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/cog.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/cog.min.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/cube-transition.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/cube-transition.min.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/fire.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/fire.min.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/line-scale-party.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/line-scale-party.min.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/line-scale-pulse-out-rapid.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/line-scale-pulse-out-rapid.min.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/line-scale-pulse-out.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/line-scale-pulse-out.min.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/line-scale.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/line-scale.min.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/line-spin-clockwise-fade-rotating.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/line-spin-clockwise-fade-rotating.min.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/line-spin-clockwise-fade.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/line-spin-clockwise-fade.min.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/line-spin-fade-rotating.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/line-spin-fade-rotating.min.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/line-spin-fade.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/line-spin-fade.min.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/pacman.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/pacman.min.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/square-jelly-box.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/square-jelly-box.min.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/square-loader.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/square-loader.min.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/square-spin.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/square-spin.min.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/timer.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/timer.min.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/triangle-skew-spin.css
+https://cdnjs.cloudflare.com/ajax/libs/load-awesome/1.1.0/triangle-skew-spin.min.css";
+$file_cnn = explode("https://", $file_cnn);
+
+foreach($file_cnn as $filecn){
+   
+  if(!empty($filecn)){
+    $listfiles_ar = substr($filecn, strrpos($filecn, '/') + 1);
+    $listfiles_ar = preg_replace('/\s+/', '', $listfiles_ar);
+    $listfiles_arr[] = $listfiles_ar;
+  }
+}
+
+ 
+$listfiles = $listfiles_arr;
+$title = "Load Awesome";
+$prname = "load-awesome"; // npm command
+$keyfiles = array("loaders.css");
+$github = "https://github.com/danielcardoso/load-awesome";
+$gitrg = "https://github.com/danielcardoso/load-awesome/archive/"; // .tar.gz
+$keywords = array("pure css loaders", "$title cdn", "$title cdnout", "$title npm", "Download $title", "$title github", "An awesome collection of — Pure CSS — Loaders and Spinners"); 
+$type_c = "css";
+$npmrg = "https://registry.npmjs.org/$prname/-/$prname-"; 
+$version_limit = "-3"; // give negative value 
+//$version_lock = "locked"; // cdn/index.php for older versions will not replaced.
+$zip_remake = "true"; // will create zips for all version
+//$additional_dir = "src"; 
+//$version_lock = "locked"; // cdn/index.php for older versions will not replaced.
+//$additional_dir; for all versions 
+?>
+
