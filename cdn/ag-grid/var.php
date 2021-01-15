3dc40d049e4322cb24378227b0ac7118e84e0db3
cdnout <cdnout@gmail.com>
2021-01-16 03:55:39 +0500

    Update var.php

--- cdn/ag-grid/var.php
+++ cdn/ag-grid/var.php
@@ -1,5 +1,5 @@
 <?php 
-//$npm_check = "";
+$npm_check = "ag-grid-community";
 $keyfiles_add = array("styles/ag-grid.min.css", "styles/ag-grid.css", "ag-grid.min.js");
 $version_limit = "-10";
 $type_s = "jquery";
