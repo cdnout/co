86fab1e19eca0679d0d7cde27f10cf7df59f7cfb
cdnout <cdnout@gmail.com>
2021-01-16 02:04:11 +0500

    Update index.php

--- index.php
+++ index.php
@@ -26,64 +26,105 @@
     <main class="container main">
        <section class="cdn-library ">
          <?php 
-          function scan_dir($dir) {
-              $ignored = array('.', '..', '.svn', '.node_modules', '.htaccess', "$dir@");
+        function scan_dir($dir) {
+        $ignored = array('.', '..', '.svn', '.node_modules', '.htaccess', "$dir@");
 
-              $files = array();    
-              foreach (scandir($dir) as $file) {
-                
-                  if (in_array($file, $ignored)) continue;
-                  $files[$file] = filemtime($dir . '/' . $file);
-                
-              }
-
-              arsort($files);
-              $files = array_keys($files);
-              return ($files) ? $files : false;
-          }
-        
-        $dirs = scan_dir("./cdn/");
-        
+        $files = array();    
+        foreach (scandir($dir) as $file) {
+          if (in_array($file, $ignored)) continue;
+          $files[$file] = filemtime($dir . '/' . $file);
+        }
+        arsort($files);
+        $files = array_keys($files);
+        return ($files) ? $files : false;
+        }        
+        $dirs = scan_dir("./zip/");
         foreach($dirs as $value){
+          $value = str_replace(".zip", "", $value);
           $url = 'cdn/'.$value.'/var.php';
-          
-          
-          if(file_exists($url)){
-            include($url);
-            $filess = glob("cdn/$value/" . '*.txt');
-            $filess2 = implode("/", $filess);
-            $filess2 = explode("/", $filess2);            
-            $version_file = end($filess2);
-            $v_h_latest = substr($version_file, 0, strpos($version_file, ".txt")); 
-            if(empty($v_h_latest)) {
-              $v_h = preg_grep("~^$prname@.*~", scandir("cdn/"));
-              $v_h_latest = end($v_h);
-              $v_h_latest = str_replace("$prname@", '', $v_h_latest);
-              $v_h_latest = str_replace(".zip", '', $v_h_latest);
-            }
+        if(file_exists($url)){
+        include($url);
+        $json_url = 'cdn/'.$value.'/package.json';
+        $json = file_get_contents($json_url);   
+        $data = json_decode($json, TRUE);
+
+foreach($data as $data_as) {
+  $director = $data_as['name'];
+  $title = str_replace("-", " ", $director);
+  $title = ucwords($title);
+  $latest_version = $data_as['version'];
+  $keyfiles = $data_as['keyfiles'];
+  $keyfiles_list = explode(",", $keyfiles);
+  $about = $data_as['description'];
+  $about = ucfirst($about);
+  $github_code = $data_as['repository'];
+  
+  if(isset($data_as['npm'])) {
+    $npm = $data_as['npm'];  
+  } else {
+    if(isset($npm_check)) {
+      $npm = $npm_check; 
+    } else {
+      $npm = "";
+    }
+  }
+  
+  $github = str_replace('git:', '', $github_code); 
+  $github = str_replace('+ssh', '', $github);
+  $github = str_replace('git+', '', $github);
+  $github = str_replace('http:', '', $github);
+  $github = str_replace('https:', '', $github);
+  $github = str_replace('.git', '', $github);
+}
+            
+if(isset($keyfiles_add)) {
+  $keyfiles_list = array_merge($keyfiles_list, $keyfiles_add);
+}
+
+$key_css_live = $key_js_live = "";
+foreach($keyfiles_list as $file_ext) {
+  $file_ext_i = explode("/", $file_ext);
+  $file_ext_filename = end($file_ext_i);
+  $file_ext_final = pathinfo($file_ext_filename, PATHINFO_EXTENSION);
+
+  if($file_ext_final == "js") {
+    $key_js_live = <<<EOD
+<script src="https://cdnout.com/$director/"></script>
+
+EOD;
+  }
+  if($file_ext_final == "css") {
+    $key_css_live = <<<EOD
+<link rel="stylesheet" href="https://cdnout.com/$director/$file_ext" media="all">
+
+EOD;
+  }
+}        
+            
           ?>
          <article class="cdn">
           <div class="text-box">            
-            <h2><a href="cdn/<?php echo $prname ?>/"><?php echo $title; ?></a></h2>
+            <h2><a href="cdn/<?php echo $director ?>/"><?php echo $title; ?></a></h2>
             <ul class="meta">
-              <li class="node"><a href="cdn/<?php echo $prname; ?>#node"><i class="icon-node"></i> Node</a></li>
-              <?php 
+              <?php if(!empty($npm)) { ?>
+              <li class="node"><a href="cdn/<?php echo $director; ?>#node"><i class="icon-node"></i> Node</a></li>
+              <?php }
               if(!empty($github)) {
               ?>
               <li><a rel="nofollow" target="_blank" href="<?php echo $github; ?>"><i class='icon-github'></i>Github</a></li>
               <?php } ?>
-              <li class="version" title="Current Version: <?php echo $v_h_latest; ?>"><span><i class="icon-layers"></i>v<?php echo $v_h_latest; ?></span></li>
+              <li class="version" title="Current Version: <?php echo $latest_version; ?>"><span><i class="icon-layers"></i>v<?php echo $latest_version; ?></span></li>
             </ul>
           </div>
           <div class="path">
             <div class="btn-block">
               
-                <button title='Copy <?php echo $prname ?> <?php echo  $v_h_latest; ?> necessary CDN Files' 
-                        data-clipboard-text='<?php foreach($keyfiles as $keyfileName){ get_file_code($keyfileName, $prname);} ?>'
+                <button title='Copy <?php echo $director ?> <?php echo  $latest_version; ?> necessary CDN Files' 
+                        data-clipboard-text='<?php echo $key_js_live.$key_css_live; ?>'
                         type="button" class="btn btn-dark copycat">
                   <i class="icon-copy"></i> Copy Key Files
                 </button>
-                <a href="cdn/<?php echo $prname ?>/" class="btn btn-light-outline"><i class="icon-code-fork"></i> View All Files</a>
+                <a href="cdn/<?php echo $director ?>/" class="btn btn-light-outline"><i class="icon-code-fork"></i> View All Files</a>
               </div>
             
           </div>
