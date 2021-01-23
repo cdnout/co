3b406985a08f080dae9060b6a0b84bab70063762
cdnout <cdnout@gmail.com>
2021-01-24 04:08:50 +0500

    co_finalized_beta_launch

---
+++ ZzFX/ZzFX.js
@@ -0,0 +1,422 @@
+/*
+
+ZzFX - Zuper Zmall Zound Zynth
+By Frank Force 2019
+
+ZzFX Features
+
+- Tiny synth engine with 17 controllable parameters.
+- Play sounds via code, no need for sound assed files!
+- Compatible with nearly all web browsers.
+- Small code footprint, the micro version is under 1 kilobyte.
+- Can produce a huge variety of sound effect types.
+- Sounds can be played with a short call. zzfx(...[,,,,.1,,,,9])
+- A small bit of randomness appied to sounds when played.
+- Use ZZFX.GetNote to get frequencies on a standard diatonic scale.
+- Sounds can be saved out as wav files for offline playback.
+- Both ZzFX and ZzFXMicro run in strict mode.
+- No additional libraries or dependencies are required.
+- Open source with MIT license, you can use this for anything!
+
+*/
+/*
+
+  ZzFX MIT License
+  
+  Copyright (c) 2019 - Frank Force
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+  
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+  
+*/
+
+'use strict';
+
+// ==ClosureCompiler==
+// @compilation_level ADVANCED_OPTIMIZATIONS
+// @output_file_name zzfx.min.js
+// @js_externs zzfx, ZZFX, _ZZFX.samples, _ZZFX.volume
+// @js_externs _ZZFX, _ZZFX.Play, _ZZFX.PlaySamples, _ZZFX.BuildSamples
+// @js_externs _ZZFX.BuildRandomSound, _ZZFX.BuildSound, _ZZFX.GetNote
+// @js_externs _ZZFX.SoundToArray, _ZZFX.CreateAudioContext, _ZZFX.randomness
+// @language_out ECMASCRIPT_2019
+// ==/ClosureCompiler==
+
+class _ZZFX
+{
+
+constructor()
+{
+    this.x = this.CreateAudioContext(); // shared audio context
+    this.volume = .3;                   // master volume scale
+    this.sampleRate = 44100;            // sample rate for audio
+    this.samples = 0;                   // last played samples
+}
+
+Play(sound)
+{
+    // check if sound object was passed in
+    const params = sound && typeof sound == 'object' ? 
+        this.SoundToArray(sound) : arguments;
+
+    // build samples and start sound
+    const samples = this.BuildSamples(...params);
+    return this.PlaySamples(samples);
+}
+
+PlaySamples(samples)
+{
+    // play an array of audio samples
+    const buffer = this.x.createBuffer(1, samples.length, this.sampleRate);
+    const source = this.x.createBufferSource();
+    buffer.getChannelData(0).set(samples);
+    source.buffer = buffer;
+    source.connect(this.x.destination);
+    source.start();
+    this.samples = samples;
+    return source;
+}
+
+BuildSamples
+(
+    volume = 1, 
+    randomness = .05,
+    frequency = 220,
+    attack = 0,
+    sustain = 0,
+    release = .1,
+    shape = 0,
+    shapeCurve = 1,
+    slide = 0, 
+    deltaSlide = 0, 
+    pitchJump = 0, 
+    pitchJumpTime = 0, 
+    repeatTime = 0, 
+    noise = 0,
+    modulation = 0,
+    bitCrush = 0,
+    delay = 0,
+    sustainVolume = 1,
+    decay = 0
+)
+{
+    // init parameters
+    let PI2 = Math.PI*2,
+    sampleRate = this.sampleRate,
+    sign = v => v>0?1:-1,
+    startSlide = slide *= 500 * PI2 / sampleRate**2,
+    modPhase = sign(modulation) * PI2/4,
+    startFrequency = frequency *= (1 + randomness*2*Math.random() - randomness) 
+        * PI2 / sampleRate;
+        
+    // scale by sample rate
+    attack = 99 + attack * sampleRate; // soften attack
+    decay = decay * sampleRate;
+    sustain = sustain * sampleRate;
+    release = release * sampleRate;
+    delay = delay * sampleRate;
+    deltaSlide *= 500 * PI2 / sampleRate**3;
+    modulation *= PI2 / sampleRate;
+    pitchJump *= PI2 / sampleRate;
+    pitchJumpTime *= sampleRate;
+    repeatTime *= sampleRate;
+    const length = attack + decay + sustain + release + delay;
+
+    // generate waveform
+    let b=[], t=0, tm=0, i=0, j=1, r=0, c=0, s=0;
+    for(; i < length; b[i++] = s)
+    {
+        if (++c>bitCrush*100)                            // bit crush
+        {
+            c = 0;                                       // reset bit crush
+            s = t * frequency *                          // frequency
+                Math.sin(tm * modulation - modPhase);    // modulation
+
+            s = shape? shape>1? shape>2? shape>3?        // wave shape
+                Math.sin((s%PI2)**3) :                   // 4 noise
+                Math.max(Math.min(Math.tan(s),1),-1):    // 3 tan
+                1-(2*s/PI2%2+2)%2:                       // 2 saw
+                1-4*Math.abs(Math.round(s/PI2)-s/PI2):   // 1 triangle
+                Math.sin(s);                             // 0 sin
+            s = sign(s)*(Math.abs(s)**shapeCurve);       // curve 0=square, 2=pointy
+
+            s *= volume * this.volume * (                // envelope
+                i < attack ? i/attack :                  // attack
+                i < attack + decay ?                     // decay
+                1-((i-attack)/decay)*(1-sustainVolume) : // decay falloff
+                i < attack  + decay + sustain ?          // sustain
+                sustainVolume :                          // sustain volume
+                i < length - delay ?                     // release
+                (length - i - delay)/release *           // release falloff
+                sustainVolume :                          // release volume
+                0);                                      // post release
+
+            s = delay ? s/2 + (delay > i ? 0 :           // delay
+                (i<length-delay? 1 : (i-length)/delay) * // release delay 
+                b[i-delay|0]/2) : s;                     // sample delay
+        }
+
+        t += 1 - noise + (Math.sin(i)+1)*1e9%2*noise;     // noise
+        tm += 1 - noise + (Math.sin(i)**2+1)*1e9%2*noise; // modulation noise
+        frequency += slide += deltaSlide;                 // frequency slide
+
+        if (j && ++j > pitchJumpTime)                // pitch jump
+        {
+            frequency += pitchJump;                  // apply pitch jump
+            startFrequency += pitchJump;             // also apply to start
+            j = 0;                                   // reset pitch jump time
+        }
+
+        if (repeatTime && ++r > repeatTime)           // repeat
+        {
+            frequency = startFrequency;               // reset frequency
+            slide = startSlide;                       // reset slide
+            r = 1;                                    // reset repeat time
+            j = j || 1;                               // reset pitch jump time
+        }
+    }
+
+    return b;
+}
+
+BuildRandomSound(lengthScale=1, volume=1, randomness=.05)
+{
+    // generate a random sound
+    const R=()=>Math.random(), C=()=>R()<.5?R():0, S=()=>C()?1:-1,
+
+    // randomize sound length
+    attack  = R()**3/4*lengthScale,
+    decay   = R()**3/4*lengthScale,
+    sustain = R()**3/4*lengthScale,
+    release = R()**3/4*lengthScale,
+    length  = attack + decay + sustain + release;
+
+    // create random sound
+    return this.BuildSound
+    (
+       volume,           // volume
+       randomness,       // randomness
+       R()**2*2e3,       // frequency
+       attack,           // attack
+       sustain,          // sustain
+       release,          // release
+       R()*5|0,          // shape
+       R()**2*3,         // shapeCurve
+       C()**3*99*S(),    // slide
+       C()**3*99*S(),    // deltaSlide
+       C()**2*1e3*S(),   // pitchJump
+       R()**2 * length,  // pitchJumpTime
+       C() * length,     // repeatTime
+       C()**4,           // noise
+       C()**3*9*S(),     // modulation
+       C()**4,           // bitCrush
+       C()**3/2,         // delay
+       1 - C(),          // sustain volume
+       decay             // decay
+    );
+}
+
+BuildSound
+(
+    volume = 1, 
+    randomness = .05,
+    frequency = 220,
+    attack = 0,
+    sustain = 0,
+    release = .1,
+    shape = 0,
+    shapeCurve = 1,
+    slide = 0, 
+    deltaSlide = 0, 
+    pitchJump = 0, 
+    pitchJumpTime = 0, 
+    repeatTime = 0, 
+    noise = 0,
+    modulation = 0,
+    bitCrush = 0,
+    delay = 0,
+    sustainVolume = 1,
+    decay = 0
+)
+{
+    // build sound object
+    const sound = 
+    {
+        'volume':       volume,
+        'randomness':   randomness,
+        'frequency':    frequency,
+        'attack':       attack,
+        'sustain':      sustain,
+        'release':      release,
+        'shape':        shape,
+        'shapeCurve':   shapeCurve,
+        'slide':        slide,
+        'deltaSlide':   deltaSlide,
+        'pitchJump':    pitchJump,
+        'pitchJumpTime':pitchJumpTime,
+        'repeatTime':   repeatTime,
+        'noise':        noise,
+        'modulation':   modulation,
+        'bitCrush':     bitCrush,
+        'delay':        delay,
+        'sustainVolume':sustainVolume,
+        'decay':        decay
+    };
+
+    return sound;
+}
+
+GetNote(semitoneOffset=0, rootNoteFrequency=440)
+{
+    // get frequency of a musical note on a diatonic scale
+    return rootNoteFrequency * 2**(semitoneOffset/12);
+}
+
+SoundToArray(sound)
+{
+    // convert sound parameters object to array
+    // use default sound for keys and order
+    const defaultSound = this.BuildSound();
+    const array = [];
+    for(const key in defaultSound)
+        array.push(sound[key]);
+    return array;
+}
+
+CreateAudioContext()
+{
+    // fix compatibility issues with old web audio
+    const audioContext = new (AudioContext || webkitAudioContext);
+    audioContext._createBufferSource = audioContext.createBufferSource;
+    audioContext.createBufferSource =
+    (s = audioContext._createBufferSource())=>
+    (
+        s.start = s.start || (t => audioContext.noteOn (t)),
+        s.stop  = s.stop  || (t => audioContext.noteOff(t)),
+        s
+    );
+
+    return audioContext;
+}
+
+} // class _ZZFX
+
+const ZZFX = new _ZZFX;
+function zzfx() { return ZZFX.Play(...arguments) }
+
+///////////////////////////////////////////////////////////////////////////////
+
+// ZzFXMicro - super tiny version with only a function to play sounds
+
+// ==ClosureCompiler==
+// @compilation_level ADVANCED_OPTIMIZATIONS
+// @output_file_name zzfx.micro.js
+// @js_externs zzfxP, zzfxV, zzfxX
+// @language_out ECMASCRIPT_2019
+// ==/ClosureCompiler==
+
+let zzfxV = .3;   // volume
+const zzfxP =     // play a sound
+(
+    // parameters
+    volume = 1, randomness = .05, frequency = 220, attackIn = 0, sustainIn = 0, releaseIn = .1, shape = 0, shapeCurve = 1, slide = 0, deltaSlide = 0, pitchJump = 0, pitchJumpTime = 0, repeatTime = 0, noise = 0, modulation = 0, bitCrush = 0, delayIn = 0, sustainVolume = 1, decayIn = 0, sampleRate = 44100,
+    
+    // init parameters and helper functions
+    attack = 99 + attackIn * sampleRate, // soften attack
+    sustain = sustainIn * sampleRate,
+    release = releaseIn * sampleRate,
+    decay = decayIn * sampleRate,
+    delay = delayIn * sampleRate,
+    PI2 = Math.PI*2,
+    sign =v=> v>0? 1 : -1,
+    length = attack + decay + sustain + release + delay,
+    startSlide = slide *= 500 * PI2 / sampleRate**2,
+    startFrequency = frequency *= (1 + randomness*2*Math.random() - randomness) 
+        * PI2 / sampleRate,
+    modPhase = sign(modulation) * PI2/4,
+    t=0, tm=0, i=0, r=0, c=0, s=0, j=1, b = [],
+    source = zzfxX.createBufferSource(), 
+    buffer = zzfxX.createBuffer(1, length, sampleRate)
+)=>
+{
+    // loop and generate waveform
+    for(source.connect(zzfxX.destination); i < length; b[i++] = s)
+    {
+        if (++c>bitCrush*100)                            // bit crush
+        {
+            c = 0;                                       // reset bit crush
+            s = t * frequency *                          // frequency
+                Math.sin(tm * modulation * PI2 / sampleRate - modPhase); // modulation
+
+            s = shape? shape>1? shape>2? shape>3?        // wave shape
+                Math.sin((s%PI2)**3) :                   // 4 noise
+                Math.max(Math.min(Math.tan(s), 1), -1):  // 3 tan
+                1-(2*s/PI2%2+2)%2:                       // 2 saw
+                1-4*Math.abs(Math.round(s/PI2)-s/PI2):   // 1 triangle
+                Math.sin(s);                             // 0 siny
+
+            s = sign(s) * (Math.abs(s)**shapeCurve) *       // curve 0=square
+                volume * zzfxV * (                          // envelope
+                    i < attack ? i/attack :                 // attack
+                    i < attack + decay ?                    // decay
+                    1-((i-attack)/decay)*(1-sustainVolume) :// decay falloff
+                    i < attack + decay + sustain ?          // sustain
+                    sustainVolume :                         // sustain volume
+                    i < length - delay ?                    // release
+                    (length - i - delay)/release *          // release falloff
+                    sustainVolume :                         // release volume
+                0);                                         // post release
+
+            s = delay ? s/2 + (delay > i ? 0 :           // delay
+                (i<length-delay? 1 : (i-length)/delay) * // release delay 
+                b[i - delay|0]/2) : s;                   // sample delay
+        }
+
+        t += 1 - noise + (Math.sin(i)+1)*1e9%2*noise;     // noise
+        tm += 1 - noise + (Math.sin(i)**2+1)*1e9%2*noise; // modulation noise
+        frequency += slide += deltaSlide             // frequency slide
+            * 500 * PI2 / sampleRate**3;             // apply sample rate
+
+        if (j && ++j > pitchJumpTime * sampleRate)   // pitch jump
+        {
+            frequency += pitchJump                   // apply pitch jump
+                * PI2 / sampleRate;                  // apply sample rate
+            startFrequency += pitchJump              // also apply to start
+                * PI2 / sampleRate;                  // apply sample rate
+            j = 0;                                   // reset pitch jump time
+        }
+
+        if (repeatTime && ++r > repeatTime*sampleRate)// repeat
+        {
+            frequency = startFrequency;               // reset frequency
+            slide = startSlide;                       // reset slide
+            r = 1;                                    // reset repeat time
+            j = j || 1;                               // reset pitch jump time
+        }
+    }
+
+    // create buffer and set source
+    buffer.getChannelData(0).set(b);
+    source.buffer = buffer;
+    source.start();
+    return source;
+}
+const zzfxX = new AudioContext;
+
+// fix compatibility issues with old web audio (optional)
+//zzfxX=new(window.AudioContext||webkitAudioContext);zzfxX.Z=zzfxX.createBufferSource;zzfxX.createBufferSource=(s=zzfxX.Z())=>(s.start=s.start||(t=>zzfxX.noteOn(t)),s.stop=s.stop||(t=>zzfxX.noteOff(t)),s)
---
+++ zingchart-react/zingchart-react.js
@@ -0,0 +1,452 @@
+'use strict';
+
+function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
+
+var React = require('react');
+var React__default = _interopDefault(React);
+
+var EVENT_NAMES = [
+  'about_hide',
+  'about_show',
+  'animation_end',
+  'animation_start',
+  'animation_step',
+  'beforedestroy',
+  'bugreport_hide',
+  'bugreport_show',
+  'click',
+  'complete',
+  'data_export',
+  'dataexport',
+  'dataload',
+  'dataparse',
+  'dataready',
+  'destroy',
+  'dimension_change',
+  'error',
+  'feed_clear',
+  'feed_interval_modify',
+  'feed_start',
+  'feed_stop',
+  'gcomplete',
+  'gload',
+  'gparse',
+  'guide_mousemove',
+  'guide_mouseout',
+  'guide_mouseout',
+  'heatmap.mousemove',
+  'history_back',
+  'history_forward',
+  'image_save',
+  'label_click',
+  'label_mousedown',
+  'label_mouseout',
+  'label_mouseover',
+  'label_mouseup',
+  'legend_hide',
+  'legend_item_click',
+  'legend_item_mousemove',
+  'legend_item_mouseout',
+  'legend_item_mouseout',
+  'legend_item_mouseover',
+  'legend_marker_click',
+  'legend_marker_click',
+  'legend_maximize',
+  'legend_minimize',
+  'legend_minimize_click',
+  'legend_pagination_click',
+  'legend_show',
+  'legend-drag_mousedown',
+  'lens_hide',
+  'lens_show',
+  'load',
+  'maps.zoom',
+  'menu_item_click',
+  'modify',
+  'modulesready',
+  'mousewheel',
+  'node_add',
+  'node_click',
+  'node_deselect',
+  'node_doubleclick',
+  'node_mousedown',
+  'node_mouseout',
+  'node_mouseover',
+  'node_mouseup',
+  'node_remove',
+  'node_select',
+  'node_set',
+  'objectsinit',
+  'objectsready',
+  'overscroll',
+  'plot_add',
+  'plot_click',
+  'plot_deselect',
+  'plot_doubleclick',
+  'plot_hide',
+  'plot_modify',
+  'plot_mouseout',
+  'plot_mouseover',
+  'plot_remove',
+  'plot_select',
+  'plot_show',
+  'postzoom',
+  'print',
+  'reload',
+  'render',
+  'resize',
+  'setdata',
+  'shape_click',
+  'shape_mousedown',
+  'shape_mouseout',
+  'shape_mouseover',
+  'shape_mouseup',
+  'source_hide',
+  'source_show',
+  'swipe',
+  'touchemove',
+  'touchend',
+  'touchstart',
+  'zingchart.plugins.selection-tool.mouseup',
+  'zingchart.plugins.selection-tool.selection',
+  'zoom'
+];
+
+var METHOD_NAMES = [
+  'addgraph',
+  'addmenuitem',
+  'addnode',
+  'addnote',
+  'addobject',
+  'addplot',
+  'addrule',
+  'addscalevalue',
+  'appendseriesdata',
+  'appendseriesvalues',
+  'clearfeed',
+  'clearscroll',
+  'clearselection',
+  'clicknode',
+  'clicknode',
+  'closemodal',
+  'closemodal',
+  'destroy/zcdestroy',
+  'disable',
+  'downloadCSV',
+  'downloadRAW',
+  'downloadXLS',
+  'exitfullscreen',
+  'exportdata',
+  'exportimage',
+  'fullscreen',
+  'get3dview',
+  'getbubblesize',
+  'getcharttype',
+  'getdata',
+  'getgraphlength',
+  'getimagedata',
+  'getinterval',
+  'getnodelength',
+  'getnodevalue',
+  'getobjectinfo',
+  'getoriginaljson',
+  'getpage',
+  'getplotlength',
+  'getplotvalues',
+  'getrender',
+  'getrules',
+  'getscaleinfo',
+  'getscales',
+  'getselection',
+  'getseriesdata',
+  'getseriesdata',
+  'getseriesvalues',
+  'getversion',
+  'getxyinfo',
+  'goback',
+  'goforward',
+  'hideguide',
+  'hidemenu',
+  'hideplot/plothide',
+  'hidetooltip',
+  'legendmaximize',
+  'legendminimize',
+  'legendscroll',
+  'load',
+  'loadstorage',
+  'locktooltip',
+  'mapdata',
+  'mapdata',
+  'modify',
+  'modifyplot',
+  'openmodal',
+  'print',
+  'reload',
+  'removegraph',
+  'removenode',
+  'removenote',
+  'removeobject',
+  'removeplot',
+  'removerule',
+  'removescalevalue',
+  'repaintobjects',
+  'resize',
+  'saveasimage',
+  'set3dview',
+  'setcharttype',
+  'setdata',
+  'setguide',
+  'setinterval',
+  'setmode',
+  'setnodevalue',
+  'setpage',
+  'setscalevalues',
+  'setselection',
+  'setseriesdata',
+  'setseriesvalues',
+  'showhoverstate',
+  'showmenu',
+  'showplot/plotshow',
+  'showtooltip',
+  'startfeed',
+  'stopfeed',
+  'togglebugreport',
+  'toggledimension',
+  'togglelegend',
+  'toggleplot',
+  'togglesource',
+  'unbinddocument',
+  'unlocktooltip',
+  'update',
+  'updatenote',
+  'updateobject',
+  'updaterule',
+  'viewDataTable',
+  'zoomin',
+  'zoomout',
+  'zoomto',
+  'zoomtovalues'
+];
+
+var MARKER_NAMES = [
+  'square',
+  'parallelogram',
+  'trapezoid',
+  'circle',
+  'diamond',
+  'triangle',
+  'ellipse',
+  'star5',
+  'star6',
+  'star7',
+  'star8',
+  'rpoly5',
+  'rpoly6',
+  'rpoly7',
+  'rpoly8',
+  'gear5',
+  'gear6',
+  'gear7',
+  'gear8',
+  'pie',
+];
+
+var MISC = {
+  DEFAULT_WIDTH: '100%',
+  DEFAULT_HEIGHT: 480,
+  DEFAULT_OUTPUT: 'svg',
+};
+
+const {DEFAULT_WIDTH, DEFAULT_HEIGHT, DEFAULT_OUTPUT} = MISC;
+
+var constants = {
+  EVENT_NAMES,
+  METHOD_NAMES,
+  MARKER_NAMES,
+  DEFAULT_WIDTH,
+  DEFAULT_HEIGHT,
+  DEFAULT_OUTPUT,
+};
+
+var classCallCheck = function (instance, Constructor) {
+  if (!(instance instanceof Constructor)) {
+    throw new TypeError("Cannot call a class as a function");
+  }
+};
+
+var createClass = function () {
+  function defineProperties(target, props) {
+    for (var i = 0; i < props.length; i++) {
+      var descriptor = props[i];
+      descriptor.enumerable = descriptor.enumerable || false;
+      descriptor.configurable = true;
+      if ("value" in descriptor) descriptor.writable = true;
+      Object.defineProperty(target, descriptor.key, descriptor);
+    }
+  }
+
+  return function (Constructor, protoProps, staticProps) {
+    if (protoProps) defineProperties(Constructor.prototype, protoProps);
+    if (staticProps) defineProperties(Constructor, staticProps);
+    return Constructor;
+  };
+}();
+
+var inherits = function (subClass, superClass) {
+  if (typeof superClass !== "function" && superClass !== null) {
+    throw new TypeError("Super expression must either be null or a function, not " + typeof superClass);
+  }
+
+  subClass.prototype = Object.create(superClass && superClass.prototype, {
+    constructor: {
+      value: subClass,
+      enumerable: false,
+      writable: true,
+      configurable: true
+    }
+  });
+  if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass;
+};
+
+var possibleConstructorReturn = function (self, call) {
+  if (!self) {
+    throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
+  }
+
+  return call && (typeof call === "object" || typeof call === "function") ? call : self;
+};
+
+var DEFAULT_WIDTH$1 = constants.DEFAULT_WIDTH,
+    DEFAULT_HEIGHT$1 = constants.DEFAULT_HEIGHT,
+    DEFAULT_OUTPUT$1 = constants.DEFAULT_OUTPUT,
+    EVENT_NAMES$1 = constants.EVENT_NAMES,
+    METHOD_NAMES$1 = constants.METHOD_NAMES;
+
+// One time setup globally to handle all zingchart-react objects in the app space.
+
+if (!window.ZCReact) {
+  window.ZCReact = {
+    instances: {},
+    count: 0
+  };
+}
+
+var ZingChart = function (_Component) {
+  inherits(ZingChart, _Component);
+
+  function ZingChart(props) {
+    classCallCheck(this, ZingChart);
+
+    var _this = possibleConstructorReturn(this, (ZingChart.__proto__ || Object.getPrototypeOf(ZingChart)).call(this, props));
+
+    _this.id = _this.props.id || 'zingchart-react-' + window.ZCReact.count++;
+    console.log(props);
+    // Bind all methods available to zingchart to be accessed via Refs.
+    METHOD_NAMES$1.forEach(function (name) {
+      _this[name] = function (args) {
+        return window.zingchart.exec(_this.id, name, args);
+      };
+    });
+    _this.state = {
+      style: {
+        height: _this.props.height || DEFAULT_HEIGHT$1,
+        width: _this.props.width || DEFAULT_WIDTH$1
+      }
+    };
+    return _this;
+  }
+
+  createClass(ZingChart, [{
+    key: 'render',
+    value: function render() {
+      return React__default.createElement('div', { id: this.id, style: this.state.style });
+    }
+  }, {
+    key: 'componentDidMount',
+    value: function componentDidMount() {
+      var _this2 = this;
+
+      // Bind all events registered.
+      Object.keys(this.props).forEach(function (eventName) {
+        if (EVENT_NAMES$1.includes(eventName)) {
+          // Filter through the provided events list, then register it to zingchart.
+          window.zingchart.bind(_this2.id, eventName, function (result) {
+            _this2.props[eventName](result);
+          });
+        }
+      });
+
+      this.renderChart();
+    }
+
+    // Used to check the values being passed in to avoid unnecessary changes.
+
+  }, {
+    key: 'shouldComponentUpdate',
+    value: function shouldComponentUpdate(nextProps) {
+      // Data change
+      if (JSON.stringify(nextProps.data) !== JSON.stringify(this.props.data)) {
+        zingchart.exec(this.id, 'setdata', {
+          data: nextProps.data
+        });
+
+        // Series change
+      } else if (JSON.stringify(nextProps.series) !== JSON.stringify(this.props.series)) {
+        zingchart.exec(this.id, 'setseriesdata', {
+          graphid: 0,
+          plotindex: 0,
+          data: nextProps.series
+        });
+
+        // Resize
+      } else if (nextProps.width !== this.props.width || nextProps.height !== this.props.height) {
+        this.setState({
+          style: {
+            width: nextProps.width || DEFAULT_WIDTH$1,
+            height: nextProps.height || DEFAULT_HEIGHT$1
+          }
+        });
+        zingchart.exec(this.id, 'resize', {
+          width: nextProps.width || DEFAULT_WIDTH$1,
+          height: nextProps.height || DEFAULT_HEIGHT$1
+        });
+      }
+
+      // React should never re-render since ZingChart controls this component.
+      return false;
+    }
+  }, {
+    key: 'renderChart',
+    value: function renderChart() {
+      var _this3 = this;
+
+      var renderObject = {};
+      Object.keys(this.props).forEach(function (prop) {
+        renderObject[prop] = _this3.props[prop];
+      });
+      // Overwrite some existing props.
+      renderObject.id = this.id;
+      renderObject.width = this.props.width || DEFAULT_WIDTH$1;
+      renderObject.height = this.props.height || DEFAULT_HEIGHT$1;
+      renderObject.data = this.props.data;
+      renderObject.output = this.props.output || DEFAULT_OUTPUT$1;
+
+      if (this.props.series) {
+        renderObject.data.series = this.props.series;
+      }
+      if (this.props.theme) {
+        renderObject.defaults = this.props.theme;
+      }
+      zingchart.render(renderObject);
+    }
+  }, {
+    key: 'componentWillUnmount',
+    value: function componentWillUnmount() {
+      zingchart.exec(this.id, 'destroy');
+    }
+  }]);
+  return ZingChart;
+}(React.Component);
+
+module.exports = ZingChart;
+//# sourceMappingURL=zingchart-react.js.map
