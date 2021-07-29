ecbd16a6db267cbebe3af5ed035ef81f98feb811
cdnout <cdnout@gmail.com>
2021-07-29 22:31:03 +0500

    CSS Listing Bug Fixed

--- cdn/twitter-bootstrap/var.php
+++ cdn/twitter-bootstrap/var.php
@@ -1,6 +1,7 @@
 <?php 
 //$npm_check = "";
 $keyfiles_add = array("css/bootstrap.min.css");
-$version_limit = "-25";
+$version_limit = "-28";
+$github_master_word = "refs/heads/main"; 
 //$type_s = "jquery";
 ?>
