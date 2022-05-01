ff638636ee3faa71db4e648ad2196c07b6c85805
cdnout <cdnout@gmail.com>
2022-05-01 20:33:08 +0500

    CO_Production_Head

--- meta/_head.php
+++ meta/_head.php
@@ -1,12 +1,14 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<?php
-$whitelist = array(
-    '127.0.0.1',
-    '::1'
-);
-if (!in_array($_SERVER['REMOTE_ADDR'], $whitelist)) { ?>
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-174652325-1"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
 
-<?php } ?>
+    gtag('config', 'UA-174652325-1'); 
+</script>
+
+
 <?php
 $url = "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 header("Access-Control-Allow-Origin: *");
