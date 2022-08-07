e8c6521743165b71c284e0f9e99ea0c5fe5f9f6c
m__awais <muhammad.awais@leadpops.com>
2022-08-08 00:26:06 +0500

    add cookies message

--- js/js.js
+++ js/js.js
@@ -1,3 +1,66 @@
+// cookies
+
+function pureFadeIn(elem, display){
+    var el = document.getElementById(elem);
+    el.style.opacity = 0;
+    el.style.display = display || "block";
+
+    (function fade() {
+        var val = parseFloat(el.style.opacity);
+        if (!((val += .02) > 1)) {
+            el.style.opacity = val;
+            requestAnimationFrame(fade);
+        }
+    })();
+}
+function pureFadeOut(elem){
+    var el = document.getElementById(elem);
+    el.style.opacity = 1;
+
+    (function fade() {
+        if ((el.style.opacity -= .02) < 0) {
+            el.style.display = "none";
+        } else {
+            requestAnimationFrame(fade);
+        }
+    })();
+}
+
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+function eraseCookie(name) {
+    document.cookie = name+'=; Max-Age=-99999999;';
+}
+
+function cookieConsent() {
+    if (!getCookie('purecookieDismiss')) {
+        pureFadeIn("cookieConsentContainer");
+    }
+}
+function purecookieDismiss() {
+    setCookie('purecookieDismiss','1',7);
+    pureFadeOut("cookieConsentContainer");
+}
+
+cookieConsent();
+
 /*!
  * clipboard.js v2.0.6
  * https://clipboardjs.com/
--- meta/_header.php
+++ meta/_header.php
@@ -1,4 +1,3 @@
-<div class="palestineAlert"><img src="<?php echo $base_url; ?>images/palestine-flag.png" alt="We Stand with Palestine" width="30" height=""><p>We stand with Palestine against Israeil's Military Oppression over innocent MUSLIMS.</p></div>
 <span class="text-alert"><i class="icon-check"></i>Selected CDN Resource Copied successfully!</span>
 <div class="box-alert">
     <span class="title">Be Careful!</span>
@@ -6,6 +5,14 @@
     <p class="text-red">We do not recommend Live First version for production use.</p>
     <p><strong><a href="<?php echo $base_url; ?>version-cdn/">Click here</a> for version based CDN.</strong></p>
     <a href="javascript:;" class="btn btnlive-okay">I'm Okay with Live First CDN</a>
+</div>
+<div class="cookies cookieConsentContainer" id="cookieConsentContainer" style="opacity: 1; display: block;">
+    <strong class="subtitle">Cookies</strong>
+    <p>This website uses cookies, We and our partners use technology such as cookies to analyse our traffic and to show you personalised content and ads. You consent to our cookies by clicking "Agree" or by continuing to use our website.</p>
+    <div class="btn-block">
+        <a class="btn btn-secondary" onclick="purecookieDismiss();">Agree</a>
+        <a class="link" href="https://cdnout.com/privacy.php">Cookies Policy</a>
+    </div>
 </div>
 <span class="alert-overlay btnlive-okay"></span>
 <header id="masthead">
